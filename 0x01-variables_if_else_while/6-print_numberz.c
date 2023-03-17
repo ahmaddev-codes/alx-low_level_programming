033ceabb8f2c49b943d754152e298d1e67ee47ce
iAhm-codes <omegabliss23@gmail.com>
2023-03-17 12:27:08 -0700

    Update: added code for 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,9 +10,8 @@
 int main(void)
 {
 	int i;
-
 	for (i = '0'; i <= '9' ; i++)
-                putchar(i);
-        putchar('\n');
-        return (0);
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
