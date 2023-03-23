fa2e5e046ae024d02ac0b6ababb87d6cba80359e
iAhm-codes <omegabliss23@gmail.com>
2023-03-23 14:35:33 +0100

    Upadte: added betty.doc commenting style

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
 /**
- * A  function that multiplies two integers
- *
- */
+  * mul - multiplies two integers
+  *
+  * Description: Takes two integers a and b, and return their multiple
+  *
+  * @a: first digit
+  * @b: second digit
+  *
+  * Return: The multiples of a and b
+  */
 
 int mul(int a, int b)
 {
