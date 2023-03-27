051ea336df25243811423bd0c0f044fc8a370dd2
iAhm-codes <omegabliss23@gmail.com>
2023-03-28 00:08:49 +0100

    Code: added code for 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,16 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0, j;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		i++;
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
 	}
 
 	_putchar('\n');
