4877bd2fdfe6518cc7fa0357dd69a66f3e19808f
iAhm-codes <omegabliss23@gmail.com>
2023-03-27 14:57:49 +0100

    Update: removed trailing whitespace

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  *
- * @*n: pointer to an int as parameter
+ * @n: pointer to an int as parameter
  *
  * return: always returns 0
  */
@@ -13,4 +13,4 @@ void reset_to_98(int *n)
 {
 	/* modify the value of the variable pointed to */
 	*n = 98;
-}	
+}
