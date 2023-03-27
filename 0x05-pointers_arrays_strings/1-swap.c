c576115378d73b0b2aa244bb1e7f4a4ebbe86e37
iAhm-codes <omegabliss23@gmail.com>
2023-03-27 18:47:34 +0100

    Code: added code for 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: value of first parameter
+ * @b: value of second parameter
+ *
+ * return: always returns 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
