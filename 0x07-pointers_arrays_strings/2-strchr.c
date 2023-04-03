eb4997251026ba489b8fc28a15fed73be48e8652
iAhm-codes <omegabliss23@gmail.com>
2023-04-03 23:28:31 +0100

    Code: added code for 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: The string to search
+ * @c: The character to search for
+ *
+ * Return: The first occurence of character or NULL if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0, j;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	for (j = 0; j <= i; j++)
+	{
+		if (c == s[j])
+		{
+			s += j;
+			return (s);
+		}
+	}
+}
