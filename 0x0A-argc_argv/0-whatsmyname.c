1cd461728f9f0a21f97cd91a420128edd18de755
iAhm-codes <omegabliss23@gmail.com>
2023-04-06 22:28:45 +0100

    Code: added code for 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *@argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
