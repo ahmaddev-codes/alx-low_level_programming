fef5bcf824380452d18fc29dc4f4eb7e0df2be83
iAhm-codes <omegabliss23@gmail.com>
2023-04-06 22:29:55 +0100

    Code: added code for 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	long int mul = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	else
+	{
+		for(i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+
+		printf("%ld\n", mul);
+	}
+
+	return (0);
+}
