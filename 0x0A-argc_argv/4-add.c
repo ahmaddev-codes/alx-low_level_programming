6659fc6f3a1e2f1b7789868d010cb1267d59b05d
iAhm-codes <omegabliss23@gmail.com>
2023-04-06 22:30:16 +0100

    Code: added code for 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int j, sum = 0;
+	char *c;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			c = argv[i];
+
+			for (j = 0; j < strlen(c); j++)
+			{
+				if (c[j] < 48 || c[j] > 52)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			
+			sum += atoi(c);
+			c++;
+		}
+
+		printf("%d\n", sum);
+	}
+
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
