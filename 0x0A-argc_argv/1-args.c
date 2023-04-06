3469d7518193ee1acb6137465611c36bd6511246
iAhm-codes <omegabliss23@gmail.com>
2023-04-06 22:29:09 +0100

    Code: added code for 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
