b3815d6171c5e8a23d31062c15d35f99ffcd4c15
iAhm-codes <omegabliss23@gmail.com>
2023-04-06 05:42:31 +0100

    Code: added code for 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/* Prototype for check for prime number */
+int check_prime(int n, int i);
+
+/**
+  * is_prime_number - Returns if a number is prime
+  *
+  * @n: The number to be checked
+  *
+  * Return: Prime number
+  */
+
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+/**
+  * check_prime - Check if number is prime
+  *
+  * @n: The number to be counted
+  * @i: The iteration count
+  *
+  * Return: 1 for prime or 0 composite
+  */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
+		return (1);
+
+	return (check_prime(n, i + 1));
+}
