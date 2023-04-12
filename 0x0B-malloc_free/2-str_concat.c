9803ce3cd805266e4daac09609b79429a602fbd4
iAhm-codes <omegabliss23@gmail.com>
2023-04-12 01:33:40 +0100

    Code: minimized the lines of code in a function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,33 +16,23 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	i = j = 0;
 
 	while (s1[i] != '\0')
-	{
 		i++;
-	}
 
 	while (s2[j] != '\0')
-	{
 		j++;
-	}
 
 	concat = malloc(sizeof(char) * (i + j + 1));
 
 	if (concat == NULL)
-	{
 		return (NULL);
-	}
 
 	i = j = 0;
 
