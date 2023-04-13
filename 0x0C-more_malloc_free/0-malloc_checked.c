2884df87e9ee7e8a6dfd42d6b812b3e9286d0a1f
iAhm-codes <omegabliss23@gmail.com>
2023-04-13 17:21:20 +0100

    Code: added code for 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - A function that allocates memory using malloc
+ *
+ * @b: The data type to check
+ *
+ * Return: Always return a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+
+	return (ptr);
+}
