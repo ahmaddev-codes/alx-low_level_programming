70bd71a9f35492460c59549a6dec8cfb95dd1890
iAhm-codes <omegabliss23@gmail.com>
2023-04-17 23:53:50 +0100

    Update: added README.md

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * new_dog - A function that creates new dog
+ *
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t->name = &name;
+	dog_t->age = age;
+	dog_t->owner = &owner;
+}
