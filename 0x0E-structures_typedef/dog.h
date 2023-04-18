149c83bca9c50cb67c000ef1eed21bf86ddee501
iAhm-codes <omegabliss23@gmail.com>
2023-04-18 22:04:53 +0100

    Code: added semi-colon

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,14 +10,16 @@
  * @age: Age of dog
  * @owner: Owner of dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
+/**
+ * dog_t - Typedef for dog structure
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
