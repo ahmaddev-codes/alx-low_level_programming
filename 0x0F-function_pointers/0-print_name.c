52778dd91369c38b55c8e9ea8dc0019f3eb4bd6e
iAhm-codes <omegabliss23@gmail.com>
2023-04-19 12:24:04 +0100

    Code: added code for 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name
+ * 
+ * @name: Name to change to uppercase
+ * @f: A pointer to a function that changes names to uppercase
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
