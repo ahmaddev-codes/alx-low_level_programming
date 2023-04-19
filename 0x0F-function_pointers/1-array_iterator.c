85bb89ee60731507a2e903d1a44062a8e1adf703
iAhm-codes <omegabliss23@gmail.com>
2023-04-19 19:40:35 +0100

    Code: modified file for betty code formatting

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,18 +1,19 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - A function that executes a function
+ * given as a parameter on each element of an array
  *
  * @array: The array to iterate
  * @size: Size of the array
- * @action - Pointer to the function to use
+ * @action: Pointer to the function to use
  *
  * Return: Nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i;
+	unsigned int;
 
 	for (i = 0; i < size; i++)
 	{
