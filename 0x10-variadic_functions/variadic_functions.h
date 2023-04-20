f70f943aef0c619ab4617e02f946d367f3ac54b0
iAhm-codes <omegabliss23@gmail.com>
2023-04-20 07:04:42 +0100

    Code: added files for 0x10-variadic_functions project

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VAR_FUNCTIONS
+#define VAR_FUNCTIONS
+
+#include <stdio.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
