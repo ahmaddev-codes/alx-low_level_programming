7732bd234086045d2da679cd35434008bf24030b
iAhm-codes <omegabliss23@gmail.com>
2023-04-27 21:14:15 +0100

    Code: added code for 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,16 +15,40 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
-		return (NULL);
+	if (head != NULL && str != NULL)
+	{
+		if (new_node == NULL)
+			return (NULL);
 
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
 
-	printf("[%d] %s\n", new_node->len, new_node->str);
+		printf("[%d] %s\n", new_node->len, new_node->str);
 
-	new_node->next = *head;
-	*head = new_node;
+		new_node->next = *head;
+		*head = new_node;
 
-	return (new_node);
+		return (new_node);
+	}
+
+	return (0);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @count: characters to count
+  *
+  * Return: Length of string
+  */
+int _strlen(const char *string)
+{
+	int count = 0;
+
+	while (*string)
+	{
+		string++;
+		count++;
+	}
+
+	return (count);
 }
