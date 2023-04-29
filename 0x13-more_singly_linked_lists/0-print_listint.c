361fc789517631a950675a4a9916458c7059371c
iAhm-codes <omegabliss23@gmail.com>
2023-04-29 22:17:42 +0100

    Code: changed a for loop to a while loop in 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t count = 0;
 
 	if (h != NULL)
 	{
-		for (count = 0; h != NULL; count++)
+		while (h)
 		{
 			printf("%d\n", h->n);
 			h = h->next;
+			count++;
 		}
 	}
 
