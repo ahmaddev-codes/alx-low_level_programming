5d518c5cf51813282e7d204ddc8c8a12309cce60
iAhm-codes <omegabliss23@gmail.com>
2023-04-29 22:13:11 +0100

    Code: checked if h != NULL in 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,13 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count;
 
-	for (count = 0; h != NULL; count++)
+	if (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		for (count = 0; h != NULL; count++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
 	}
 
 	return (count);
