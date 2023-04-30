26f8ba958c25fbae2bb67fe7ee5c69139f1400d8
iAhm-codes <omegabliss23@gmail.com>
2023-04-30 21:09:37 +0100

    Update: removed conditional statement since it already checks if h != NULL

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,16 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 
 	return (count);
