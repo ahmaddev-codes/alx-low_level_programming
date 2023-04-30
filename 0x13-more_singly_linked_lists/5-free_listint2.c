660dbc1d1f7d2d0465a1288ae9beab20e31c2b13
iAhm-codes <omegabliss23@gmail.com>
2023-04-30 01:44:49 +0100

    Code: removed trailing whiteline

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,16 +14,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	
+
 	if (head == NULL)
 		return;
-	
+
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	
+
 	*head = NULL;
 }
