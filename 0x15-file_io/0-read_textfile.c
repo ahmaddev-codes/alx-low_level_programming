c37e730b1698c438497302c2ff893fea052140c6
iAhm-codes <omegabliss23@gmail.com>
2023-05-09 15:50:02 +0100

    Update: removed trailing whiteline in code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,13 +21,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, bytes_read;
 	char *buffer;
-	
+
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(letters * sizeof(char));
 
-	if (buffer == NULL)							                return (0);
+	if (buffer == NULL)
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 
