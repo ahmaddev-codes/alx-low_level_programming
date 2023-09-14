9c13361a0411e167c8bb0c2e6145d17a0640f45b
iahm-codes <omegabliss23@gmail.com>
2023-09-14 18:59:30 +0100

    Code: added code to print the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list
+ *
+ * @h: The head of the doubly linked list
+ *
+ * Returns: Number of elements in the linked list 
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
