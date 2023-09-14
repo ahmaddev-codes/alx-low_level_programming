3a314e97aa56fe8653b7bf9759f5301daabd81fb
iahm-codes <omegabliss23@gmail.com>
2023-09-15 00:50:58 +0100

    Code: added code to add data to the end of a doubly linked list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add-dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ *
+ * @head: The head of the linked list
+ * @n: The data to add the linked list
+ *
+ * Return: Address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *current = *head;
+
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = n;
+	if ((*head) != NULL)
+	{
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_node;
+		new_node->prev = current;
+		new_node->next = NULL;
+	}
+
+	else
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+
+	return (*head);
+}
