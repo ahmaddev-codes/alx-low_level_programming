fdb095e0505780c693708751a06d1d267b1a2468
iahm-codes <omegabliss23@gmail.com>
2023-09-15 03:31:59 +0100

    Code: modified code to add node at a specific index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,26 +3,6 @@
 #include "lists.h"
 
 /**
- * size_dlistint - The size of the doubly linked list
- *
- * @head: The address of the head to the doubly linked list
- *
- * Return: The size of the singly linked list
- */
-unsigned int size_dlistint(dlistint_t **head)
-{
-	unsigned int size = 0;
-	dlistint_t *current = *head;
-
-	while (current != NULL)
-	{
-		current = current->next;
-		size++;
-	}
-	return (size);
-}
-
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  *
  * @h: The head of the doubly linked list
@@ -36,29 +16,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *current = *h;
-	unsigned int position = 0, size;
+	unsigned int position = 0;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	size = size_dlistint(&current);
 
-	if (idx < size)
+	if (idx == 0)
 	{
-		while (position < idx)
-		{
-			current = current->next;
-			position++;
-		}
-
-		if (current->prev != NULL)
-			current->prev->next = new_node;
-		else
-			*h = new_node;
-		new_node->prev = current->prev;
 		new_node->next = current;
-		current->prev = new_node;
+		if (current != NULL)
+			current->prev = new_node;
+		*h = new_node;
+		return (new_node);
 	}
-	return (*h);
+
+	while (current != NULL && position < idx - 1)
+	{
+		current = current->next;
+		position++;
+	}
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = new_node;
+	new_node->prev = current;
+
+	return (new_node);
 }
