e0a26dd6a8d494b71a5e7358d27dce5db5e28041
iahm-codes <omegabliss23@.com>
2023-12-12 22:32:22 +0100

    Code: corrected code for betty styling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,22 +2,34 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+  * hash_table_create - Creates a hash table
+  * @size: Size of the array
+  *
+  * Return: A pointer to the newly created hash table
+  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
-	
+	unsigned long int i;
+	hash_table_t *table = NULL;
+	hash_node_t **array = NULL;
+
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return NULL;
 
-	table->size = size;
-
-	table->array = malloc(size * sizeof(hash_node_t));
-
-	if (table->array == NULL)
+	array = malloc(size * sizeof(hash_node_t));
+	if (array == NULL)
 	{
 		free(table);
 		return NULL;
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	table->size = size;
+	table->array = array;
 
 	return table;
 }
