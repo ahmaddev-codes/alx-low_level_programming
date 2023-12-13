e376563f4e0d9dd7ee420c67489bed5ae285b96a
iahm-codes <omegabliss23@.com>
2023-12-13 19:35:27 +0100

    Code: corrected memory allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	array = malloc(size * sizeof(hash_node_t));
+	array = malloc(size * sizeof(hash_node_t *));
 	if (array == NULL)
 	{
 		free(table);
